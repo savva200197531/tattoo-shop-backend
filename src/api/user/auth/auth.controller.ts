0d4e719ead0bd71d3b8ecb8b3651fb2608daf5a4
savva200197531 <kashin.savva@mail.ru>
2022-12-24 19:15:50 +0700

    Add jwt token check. Finish register, login, refresh.

---
+++ src/api/user/auth/auth.controller.ts
@@ -0,0 +1,35 @@
+import { Body, Controller, Inject, Post, ClassSerializerInterceptor, UseInterceptors, UseGuards, Req } from '@nestjs/common';
+import { User } from '@/api/user/entities/user.entity';
+import { RegisterDto, LoginDto } from './dto/auth.dto';
+import { JwtAuthGuard } from './auth.guard';
+import { AuthService } from './auth.service';
+import { Request } from 'express';
+
+@Controller('auth')
+export class AuthController {
+  @Inject(AuthService)
+  private readonly service: AuthService;
+
+  @Post('register')
+  @UseInterceptors(ClassSerializerInterceptor)
+  private register(@Body() body: RegisterDto): Promise<User | never> {
+    return this.service.register(body);
+  }
+
+  @Post('login')
+  private login(@Body() body: LoginDto): Promise<string | never> {
+    return this.service.login(body);
+  }
+
+  @Post('logout')
+  private logout() {
+    return
+  }
+
+  @Post('refresh')
+  @UseGuards(JwtAuthGuard)
+  private refresh(@Req() { user }: Request): Promise<string | never> {
+    console.log(user);
+    return this.service.refresh(<User>user);
+  }
+}
---
+++ src/common/helper/env.helper.ts
@@ -0,0 +1,16 @@
+import { resolve } from 'path'
+import { existsSync } from 'fs'
+
+export const getEnvPath = (dest: string): string => {
+  const env: string | undefined = process.env.NODE_ENV
+  const fallback: string = resolve(`${dest}/.env`)
+  const filename: string = env ? `${env}.env` : 'development.env'
+
+  let filePath: string = resolve(`${dest}/${filename}`)
+
+  if (!existsSync(filePath)) {
+    filePath = fallback
+  }
+
+  return filePath
+}
