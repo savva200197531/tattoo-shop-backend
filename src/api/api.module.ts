97c3bbccf5d289359b6f294e4a3224abf778c56d
savva200197531 <kashin.savva@mail.ru>
2023-01-10 02:09:40 +0700

    Create cart resource. Start making products.

--- src/api/api.module.ts
+++ src/api/api.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
 import { ProductsModule } from './products/products.module';
+import { CartModule } from './cart/cart.module';
 
 @Module({
-  imports: [UserModule, ProductsModule],
+  imports: [UserModule, ProductsModule, CartModule],
 })
 export class ApiModule {}
--- src/api/products/products.controller.ts
+++ src/api/products/products.controller.ts
@@ -1,7 +1,32 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Delete, Get, Param, ParseIntPipe, Post, Put } from "@nestjs/common";
 import { ProductsService } from './products.service';
 
 @Controller('products')
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
+
+  @Post('create')
+  private create() {
+    return this.productsService.create()
+  }
+
+  @Get()
+  findAll() {
+    return this.productsService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id', ParseIntPipe) id: number) {
+    return this.productsService.findOne(id);
+  }
+
+  @Put(':id')
+  private update(@Param('id', ParseIntPipe) id: number) {
+    return this.productsService.update(id)
+  }
+
+  @Delete(':id')
+  private remove(@Param('id', ParseIntPipe) id: number) {
+    this.productsService.remove(id)
+  }
 }
